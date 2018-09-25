7ec2b594d5334b10b6d4ac69113725810f790c58
daniel.rodrigues013@gmail.com <daniel.rodrigues013@gmail.com>
2018-09-25 19:59:39 -0300

    Ajustado modal cliente.

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -18,6 +18,7 @@ import application.Main;
 import factory.JPAFactory;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -44,236 +45,196 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import model.Cliente;
 
-public class ClienteController implements Initializable{
-	
+public class ClienteController implements Initializable {
+
 	private Cliente cliente;
-	
-    @FXML
-    private TitledPane tPCliente;
-    @FXML
-    private TabPane tablePaneAbas;
-    @FXML
-    private TextField tfNome, tfCpf, tfEndereco, tfEmail;
-    @FXML
-    private Button btLimpar, btIncluir, btExcluir, btAlterar;
-    @FXML
-    private TableView<Cliente> tvClientes;
-    @FXML
-    private TableColumn<Cliente, Integer> tcIdClientes;
-    @FXML
-    private TableColumn<Cliente, String> tcCpfClientes, tcNomeClientes, tcEmailClientes, tcEnderecoClientes;
-    @FXML
-    private TextField tfPesquisar;
-    @FXML
-    private Button btPesquisar;
-    @FXML
-    private JFXButton btCadastrarCliente;
-    
+
+	@FXML
+	private TitledPane tPCliente;
+	@FXML
+	private TabPane tablePaneAbas;
+	@FXML
+	private TextField tfNome, tfCpf, tfEndereco, tfEmail;
+	@FXML
+	private Button btLimpar, btIncluir, btExcluir, btAlterar;
+	@FXML
+	private TableView<Cliente> tvClientes;
+	@FXML
+	private TableColumn<Cliente, Integer> tcIdClientes;
+	@FXML
+	private TableColumn<Cliente, String> tcCpfClientes, tcNomeClientes, tcEmailClientes, tcEnderecoClientes;
+	@FXML
+	private TextField tfPesquisar;
+	@FXML
+	private Button btPesquisar;
+	@FXML
+	private JFXButton btCadastrarCliente;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
 		tcIdClientes.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tcCpfClientes.setCellValueFactory(new PropertyValueFactory<>("cpf"));
-		tcNomeClientes.setCellValueFactory(new PropertyValueFactory<>("nome"));	
+		tcNomeClientes.setCellValueFactory(new PropertyValueFactory<>("nome"));
 		tcEmailClientes.setCellValueFactory(new PropertyValueFactory<>("email"));
 		tcEnderecoClientes.setCellValueFactory(new PropertyValueFactory<>("endereco"));
 	}
-	
-    @FXML
-    protected void dialogCliente(ActionEvent event) throws Exception {
-    	Button botao1 = new Button("Clique em mim! (Tratador externo)");
-    	Stage window = new Stage();
-    	window.initModality(Modality.APPLICATION_MODAL);
-    	
-    	//Pane pane = new Pane();
-    	Pane pane = new Pane(FXMLLoader.load(getClass().getResource("/view/hospede.fxml")));
-    	//Parent pane = FXMLLoader.load(getClass().getResource("/view/hospede.fxml"));
-    	
-    	Scene scene = new Scene(pane, 400,400);
-    	window.setScene(scene);
-    	window.initStyle(StageStyle.UNDECORATED);
-    	window.setResizable(false);
-    	window.setTitle("Janela");
-    	window.showAndWait();
 
-    } 
-    
-    @FXML
-    void handleMouseClicked(MouseEvent event) {
-    	if(event.getButton().equals(MouseButton.PRIMARY)) {
-    		if(event.getClickCount() == 2) {
-    	    	cliente = tvClientes.getSelectionModel().getSelectedItem();
-    	    	tfCpf.setText(cliente.getCpf());
-    	    	tfNome.setText(cliente.getNome());
-    	    	tfEndereco.setText(cliente.getEndereco());
-    	    	tfEmail.setText(cliente.getEmail());
-    	    	
-    	    	//selecionando a primeira Aba
-    	    	tablePaneAbas.getSelectionModel().select(0);
-    	    	
-    	    	tfCpf.requestFocus();
-    			atualizarBotoes();
-    		}
-    	}
-    }
-	
-    @FXML
-    void handleBuscar(ActionEvent event) {
-    	
-    	System.out.println("Pesquisar");
-    	EntityManager em = JPAFactory.getEntityManager();
-    	
-    	Query query = em.createQuery("Select c From Cliente c WHERE lower(c.nome) like lower(:nome)");
-    	query.setParameter("nome", "%" + tfPesquisar.getText() + "%");
-    	List<Cliente> lista = query.getResultList();
-    	    	
-    	if(lista == null || lista.isEmpty()) {
-    		Alert alerta = new Alert(AlertType.INFORMATION);
-    		alerta.setTitle("Informação");
-    		alerta.setHeaderText(null);
-    		alerta.setContentText("A lista não retornou dados");
-    		alerta.show();
-    		lista = new ArrayList<Cliente>();
-    	}
-    	
-    	
-    	
-    	tvClientes.setItems(FXCollections.observableList(lista));
-    }
-	
-    @FXML
-    void handleAlterar(ActionEvent event) {
 
-    	
-		String nome = tfNome.getText(),
-				endereco = tfEndereco.getText(),
-				cpf = tfCpf.getText(),
+	@FXML
+	private void dialogCliente(ActionEvent event) throws IOException {
+		FXMLLoader fXMLLoader = new FXMLLoader();
+		fXMLLoader.setLocation(getClass().getResource("/view/hospede.fxml"));
+		Stage stage = new Stage();
+		Scene scene = new Scene(fXMLLoader.load());
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.initStyle(StageStyle.UNDECORATED);
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setTitle("Usu�rios");
+		stage.show();
+	}
+
+	@FXML
+	void handleMouseClicked(MouseEvent event) {
+		if (event.getButton().equals(MouseButton.PRIMARY)) {
+			if (event.getClickCount() == 2) {
+				cliente = tvClientes.getSelectionModel().getSelectedItem();
+				tfCpf.setText(cliente.getCpf());
+				tfNome.setText(cliente.getNome());
+				tfEndereco.setText(cliente.getEndereco());
+				tfEmail.setText(cliente.getEmail());
+
+				// selecionando a primeira Aba
+				tablePaneAbas.getSelectionModel().select(0);
+
+				tfCpf.requestFocus();
+				atualizarBotoes();
+			}
+		}
+	}
+
+	@FXML
+	void handleBuscar(ActionEvent event) {
+		EntityManager em = JPAFactory.getEntityManager();
+
+		Query query = em.createQuery("Select c From Cliente c WHERE lower(c.nome) like lower(:nome)");
+		query.setParameter("nome", "%" + tfPesquisar.getText() + "%");
+		List<Cliente> lista = query.getResultList();
+
+		if (lista == null || lista.isEmpty()) {
+			Alert alerta = new Alert(AlertType.INFORMATION);
+			alerta.setTitle("Informação");
+			alerta.setHeaderText(null);
+			alerta.setContentText("A lista não retornou dados");
+			alerta.show();
+			lista = new ArrayList<Cliente>();
+		}
+
+		tvClientes.setItems(FXCollections.observableList(lista));
+	}
+
+	@FXML
+	void handleAlterar(ActionEvent event) {
+		String nome = tfNome.getText(), endereco = tfEndereco.getText(), cpf = tfCpf.getText(),
 				email = tfEmail.getText();
-    	
+
 		cliente.setCpf(tfCpf.getText());
 		cliente.setNome(tfNome.getText());
 		cliente.setEmail(tfEmail.getText());
 		cliente.setEndereco(tfEndereco.getText());
-		
+
 		Alert al = new Alert(AlertType.CONFIRMATION);
 		al.setHeaderText("Alterar Cadastro");
-		al.setContentText("Nome: " + nome + "\nCPF: "+cpf+"\nEndereço: "+endereco+"\nEmail: "+email);
+		al.setContentText("Nome: " + nome + "\nCPF: " + cpf + "\nEndereço: " + endereco + "\nEmail: " + email);
 		Optional<ButtonType> result = al.showAndWait();
-		if(result.get() == ButtonType.OK) {
+		if (result.get() == ButtonType.OK) {
 
-			
 			Alert alInfo = new Alert(AlertType.INFORMATION);
 			alInfo.setHeaderText("Cadastro Alterado com Sucesso!");
 			alInfo.show();
 		}
-		
+
 		EntityManager em = JPAFactory.getEntityManager();
 		em.getTransaction().begin();
 		em.merge(cliente);
 		em.getTransaction().commit();
 		em.close();
-		
+
 		handleLimpar(event);
+	}
 
-    }
-
-    @FXML
-    void handleExcluir(ActionEvent event) {
-		String nome = tfNome.getText(),
-				endereco = tfEndereco.getText(),
-				cpf = tfCpf.getText(),
+	@FXML
+	void handleExcluir(ActionEvent event) {
+		String nome = tfNome.getText(), endereco = tfEndereco.getText(), cpf = tfCpf.getText(),
 				email = tfEmail.getText();
-    	
-		
+
 		Alert al = new Alert(AlertType.CONFIRMATION);
 		al.setHeaderText("Excluir Cadastro");
-		al.setContentText("Nome: " + nome + "\nCPF: "+cpf+"\nEndereço: "+endereco+"\nEmail: "+email);
+		al.setContentText("Nome: " + nome + "\nCPF: " + cpf + "\nEndereço: " + endereco + "\nEmail: " + email);
 		Optional<ButtonType> result = al.showAndWait();
-		if(result.get() == ButtonType.OK) {
-
-			
+		if (result.get() == ButtonType.OK) {
 			Alert alInfo = new Alert(AlertType.INFORMATION);
 			alInfo.setHeaderText("Cadastro Removido com Sucesso!");
 			alInfo.show();
 		}
-		
+
 		EntityManager em = JPAFactory.getEntityManager();
 		em.getTransaction().begin();
 		cliente = em.merge(cliente);
 		em.remove(cliente);
 		em.getTransaction().commit();
 		em.close();
-		
+
 		handleLimpar(event);
-    }
+	}
 
-    @FXML
-    void handleIncluir(ActionEvent event) {
-    	
+	@FXML
+	void handleIncluir(ActionEvent event) {
+		String nome = tfNome.getText(), endereco = tfEndereco.getText(), cpf = tfCpf.getText(),
+				email = tfEmail.getText();
 
-    		String nome = tfNome.getText(),
-    				endereco = tfEndereco.getText(),
-    				cpf = tfCpf.getText(),
-    				email = tfEmail.getText();
-        	
-    		cliente = new Cliente(tfNome.getText(),tfCpf.getText(),tfEndereco.getText(),tfEmail.getText());
-    		if(nome == null || cpf == null || endereco == null) {
-    			
-    		}
-    		else {
-        		Alert al = new Alert(AlertType.CONFIRMATION);
-        		al.setHeaderText("Novo Cadastro");
-        		al.setContentText("Nome: " + nome + "\nCPF: "+cpf+"\nEndereço: "+endereco+"\nEmail: "+email);
-        		Optional<ButtonType> result = al.showAndWait();
-        		if(result.get() == ButtonType.OK) {
-        			try {
-                		EntityManager em = JPAFactory.getEntityManager();
-                		em.getTransaction().begin();
-                		em.persist(cliente);
-                		em.getTransaction().commit();
-                		em.close();
-                		
-            			Alert alInfo = new Alert(AlertType.INFORMATION);
-            			alInfo.setHeaderText("Cadastro Realizado com Sucesso!");
-            			alInfo.show();
-        			}catch (EventException e){
-        				
-        			}
+		cliente = new Cliente(tfNome.getText(), tfCpf.getText(), tfEndereco.getText(), tfEmail.getText());
+		if (nome == null || cpf == null || endereco == null) {
 
-        		}
-    		}
+		} else {
+			Alert al = new Alert(AlertType.CONFIRMATION);
+			al.setHeaderText("Novo Cadastro");
+			al.setContentText("Nome: " + nome + "\nCPF: " + cpf + "\nEndereço: " + endereco + "\nEmail: " + email);
+			Optional<ButtonType> result = al.showAndWait();
+			if (result.get() == ButtonType.OK) {
+				try {
+					EntityManager em = JPAFactory.getEntityManager();
+					em.getTransaction().begin();
+					em.persist(cliente);
+					em.getTransaction().commit();
+					em.close();
 
-    		
+					Alert alInfo = new Alert(AlertType.INFORMATION);
+					alInfo.setHeaderText("Cadastro Realizado com Sucesso!");
+					alInfo.show();
+				} catch (EventException e) {
+				}
+			}
+		}
 
-    		
+		handleLimpar(event);
+	}
 
-    		
-    		handleLimpar(event);
-
-
-
-    	
-    	
-    }
-
-    @FXML
-    void handleLimpar(ActionEvent event) {
+	@FXML
+	void handleLimpar(ActionEvent event) {
 		tfCpf.setText("");
 		tfNome.setText("");
 		tfEmail.setText("");
 		tfEndereco.setText("");
 		cliente = new Cliente();
-		
+
 		atualizarBotoes();
-    }
-	
-    private void atualizarBotoes() {
+	}
 
-        	btIncluir.setDisable(cliente.getId() != null);
-        	btAlterar.setDisable(cliente.getId() == null);
-        	btExcluir.setDisable(cliente.getId() == null);
+	private void atualizarBotoes() {
 
-
-    }
-	
+		btIncluir.setDisable(cliente.getId() != null);
+		btAlterar.setDisable(cliente.getId() == null);
+		btExcluir.setDisable(cliente.getId() == null);
+	}
 }
