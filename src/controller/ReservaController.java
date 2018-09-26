512376aca1f1a4f66981fd5135efdb74120473e8
daniel.rodrigues013@gmail.com <daniel.rodrigues013@gmail.com>
2018-09-26 16:36:35 -0300

    Tela de cadastrar reservas. Correção dos nomes de alguns componentes.

--- src/controller/ReservaController.java
+++ src/controller/ReservaController.java
@@ -1,17 +1,24 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class ReservaController implements Initializable {
 
@@ -28,11 +35,10 @@ public class ReservaController implements Initializable {
 	private TableColumn<?, ?> tcIdReserva;
 
 	@FXML
-	private TableColumn<?, ?> tcCheckin;
+	private TableColumn<?, ?> tcDataChegada;
 
 	@FXML
-	private TableColumn<?, ?> tcCheckout;
-
+	private TableColumn<?, ?> tcDataSaida;
 	@FXML
 	private TableColumn<?, ?> tcQuantidadeHospedes;
 
@@ -49,10 +55,25 @@ public class ReservaController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 
 		tcIdReserva.setCellValueFactory(new PropertyValueFactory<>("id"));
-		tcCheckin.setCellValueFactory(new PropertyValueFactory<>("dataEntrada"));
-		tcCheckout.setCellValueFactory(new PropertyValueFactory<>("dataSaida"));
+		tcDataChegada.setCellValueFactory(new PropertyValueFactory<>("dataEntrada"));
+		tcDataSaida.setCellValueFactory(new PropertyValueFactory<>("dataSaida"));
 		tcQuantidadeHospedes.setCellValueFactory(new PropertyValueFactory<>("dataSaida"));
 		tcHospedeResponsavel.setCellValueFactory(new PropertyValueFactory<>("hospedeResponsavel"));
 		tcTipoQuarto.setCellValueFactory(new PropertyValueFactory<>("tipoQuarto"));
 	}
+
+	@FXML
+	private void dialogReservas(ActionEvent event) throws IOException {
+		FXMLLoader fXMLLoader = new FXMLLoader();
+		fXMLLoader.setLocation(getClass().getResource("/view/cadastroreservas.fxml"));
+		Stage stage = new Stage();
+		Scene scene = new Scene(fXMLLoader.load());
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.initStyle(StageStyle.UNDECORATED);
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setTitle("Usuários");
+		stage.show();
+	}
+
 }
