1a468ba67195e58b9accf09861f543fa171a6143
mjassdev <matheusetf@gmail.com>
2018-09-27 13:11:39 -0300

    Adicionada tela de login.

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,15 +2,23 @@ package application;
 
 import java.net.URL;
 
+import com.jfoenix.controls.JFXButton;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application{
 
+    
+    
 	public static void main(String[] args) {
 		new Splash();
 		launch(args);
@@ -18,13 +26,16 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("/view/main_view.fxml"));
-		
-        primaryStage.setTitle("HOTEL CALIFÓRNIA");
-        primaryStage.setScene(new Scene(root, 1366, 768));
-        primaryStage.setResizable(true);
-        //primaryStage.setFullScreen(true);
+		Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));	
+        primaryStage.setTitle("HOTEL CALIFORNIA");
+        primaryStage.setScene(new Scene(root, 300, 400));
+        primaryStage.setResizable(false);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
 	}
+	
+
+
+
 
 }
