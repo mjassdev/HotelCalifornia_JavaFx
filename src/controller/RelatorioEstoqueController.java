83ac07cc1f3233c1b538d99ca8d3206c86ccd44e
mjassdev <matheusetf@gmail.com>
2018-09-27 17:26:17 -0300

    Adionando telas modulo de produtos.

---
+++ src/controller/RelatorioEstoqueController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class RelatorioEstoqueController implements Initializable{
+
+    @FXML
+    private FontAwesomeIcon btFechar;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	@FXML
+	void handleFecharJanela(MouseEvent event) {
+		Stage stage = (Stage) btFechar.getScene().getWindow(); // Obtendo a janela atual
+		stage.close(); // Fechando o Stage
+	}
+}
